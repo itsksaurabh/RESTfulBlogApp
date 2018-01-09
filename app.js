852a180763ef26090c8562ccc5b4f18b24e75e0f
kumar saurabh <itsksaurabh@gmail.com>
2018-01-09 09:56:39 +0000

    Initial deploy commit

---
+++ app.js
@@ -0,0 +1,147 @@
+// app config
+
+    var express     = require("express"),
+        app         = express(),
+        mongoose    = require("mongoose"),
+        bodyParser  = require("body-parser"),
+    methodOverride  = require("method-override");
+    
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+
+
+//Mongoose Config
+mongoose.connect("mongodb://localhost/restfulBlog");
+
+var blogSchema = new mongoose.Schema({
+    
+                 title : String,
+                 image: { type:String, default : " "},
+                 body : { type:String, default : " "},
+                 created: { type: Date, default : Date.now() }
+    
+});
+
+
+var blog = mongoose.model("blog", blogSchema);
+
+// blog.create({
+//     title :"Spiti Valley, Himachal Pradesh",
+//     image:"https://www.indianholiday.com/blog/wp-content/uploads/2014/12/Lahual-spiti.jpg",
+//     body : "Cool location"
+// })
+
+
+//Restful Routes
+
+
+//index route
+app.get("/blogs", function(req,res){
+    
+    blog.find({}, function(err, allblogs){
+        if(err){
+            console.log(err);
+        }else{
+             res.render("index", {allblogs : allblogs}); 
+        }
+        
+    })
+    
+});
+
+app.get("/", function(req,res){
+    res.redirect("/blogs");
+});
+
+//New Route
+
+app.get("/new", function(req,res){
+    
+    res.render("new");
+    
+});
+
+app.post("/blogs", function(req,res){
+   
+   blog.create(req.body.blog, function(err, newblog){
+       if(err){
+           console.log(err);
+            res.render("new");
+       }else{
+          res.redirect("/blogs");
+       }
+       
+   } )
+    
+    
+});
+
+//show one blog
+
+app.get("/blogs/:id", function(req,res){
+    
+    blog.findById(req.params.id, function(err, foundblog){
+        
+        if(err){
+            console.log(err)
+        }
+        else{
+              res.render("show", {blog: foundblog});
+        }
+    })
+ 
+});
+
+//Edit route
+app.get("/blogs/:id/edit", function(req, res){
+   
+   blog.findById(req.params.id, function(err, foundblog){
+       
+       if(err){
+          console.log(err); 
+       }
+       else{
+            res.render("edit", {blog : foundblog}) ;
+       }
+   })
+    
+});
+
+//Update route
+app.put("/blogs/:id", function(req,res){
+    
+   blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+       
+       if(err){
+           console.log(err);
+       }else{
+           
+           res.redirect("/blogs/"+ req.params.id);
+       }
+   })
+    
+});
+
+//DELETE ROUTE
+
+app.delete("/blogs/:id", function(req,res){
+    
+   blog.findByIdAndRemove(req.params.id, function(err){
+       
+       if(err){
+           res.redirect("/blogs");
+       }
+       else{
+           res.redirect("/blogs");
+       }
+       
+   }) 
+    
+});
+
+
+app.listen(process.env.PORT,process.env.IP, function(){
+    console.log("blog server started");
+});
